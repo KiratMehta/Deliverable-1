2c8e720b6c6c352d563a18c6cc51916d67ac21eb
KiratMehta <mehtkira@sheridancollege.ca>
2020-06-19 23:47:26 -0400

    CARD GAME

--- src/ca/sheridancollege/project/ExampleCardEnum.java
+++ src/ca/sheridancollege/project/ExampleCardEnum.java
@@ -1,8 +1,6 @@
-
 package ca.sheridancollege.project;
 
 import java.util.Scanner;
-
 
 public class ExampleCardEnum {
 
@@ -19,20 +17,16 @@ public class ExampleCardEnum {
         int player1Score = 0;
         int player2Score = 0;
 
-        
         GenerateCard ch = new GenerateCard();
         ch.generateCard();
-        
-     
 
-       
         GenerateCard ch2 = new GenerateCard();
         ch2.generateCard1();
-        
 
         do {
-
+            System.out.println("----INSTRUCTIONS----");
             System.out.println("Two players have been given equal amount of cards ");
+            System.out.println("The player with the greatest card wins");
             System.out.println();
 
             System.out.println("If Player 1 wants to register and play press 1 ");
@@ -41,73 +35,57 @@ public class ExampleCardEnum {
             if (play == 1) {
                 ran = (int) (Math.random() * 26);
                 System.out.println("PLAYER 1 cards: " + ch.generate[ran]);
-                System.out.println(ran);
-            }
-            else{
+
+            } else {
                 System.out.println("Player 1 doesn't want to continue, The game has ended");
                 break;
             }
             System.out.println("If Player 2 wants to register and play press 1 ");
-           
+
             int play2 = scan.nextInt();
 
             if (play2 == 1) {
                 ran2 = (int) (Math.random() * 26);
-               
-                System.out.println("PLAYER 2 cards: " + ch2.generate2[ran2]);
-                Card2 x=ch2.generate2[ran2];
-                System.out.println(x);
 
-            }
-            else{
+                System.out.println("PLAYER 2 cards: " + ch2.generate2[ran2]);
+
+            } else {
                 System.out.println("If Player 1 doesn't want to continue, The game has ended");
                 break;
             }
-                
-            if(ran<13&&ran2<13 || ran>=13&&ran2>=13)
-            {
-               if (ran > ran2) {
-                    System.out.println("ran="+ran);
-                    System.out.println("ran2="+ran2);
+
+            if (ran < 13 && ran2 < 13 || ran >= 13 && ran2 >= 13) {
+                if (ran > ran2) {
+
                     System.out.println("PLAYER 1 won");
                     player1Score = player1Score + 1;
-                } 
-            else if(ran==ran2){
-                System.out.println("ran="+ran);
-                System.out.println("ran2="+ran2);
-                System.out.println("Draw");
+                } else if (ran == ran2) {
+
+                    System.out.println("Draw");
+                } else {
+
+                    System.out.println("PLAYER 2 won");
+                    player2Score = player2Score + 1;
                 }
-                else {
-                    System.out.println("ran="+ran);
-                    System.out.println("ran2="+ran2);
+            } else if (ran < 13 && ran2 >= 13 || ran >= 13 && ran2 < 13) {
+                if (ran2 > ran) {
+                    ran2 = ran2 - 13;
+                } else {
+                    ran = ran - 13;
+                }
+                if (ran > ran2) {
+
+                    System.out.println("PLAYER 1 won");
+                    player1Score = player1Score + 1;
+                } else if (ran == ran2) {
+
+                    System.out.println("Draw");
+                } else {
+
                     System.out.println("PLAYER 2 won");
                     player2Score = player2Score + 1;
                 }
             }
-            else if(ran<13&&ran2>=13 || ran>=13&&ran2<13)
-            {
-                if(ran2>ran)
-                    ran2=ran2-13;
-                else
-                    ran=ran-13;
-                if (ran > ran2) {
-                    System.out.println("ran="+ran);
-                    System.out.println("ran2="+ran2);
-                    System.out.println("PLAYER 1 won");
-                    player1Score = player1Score + 1;
-                } 
-            else if(ran==ran2){
-                System.out.println("ran="+ran);
-                System.out.println("ran2="+ran2);
-                System.out.println("Draw");
-                }
-                else {
-                    System.out.println("ran="+ran);
-                    System.out.println("ran2="+ran2);
-                    System.out.println("PLAYER 2 won");
-                    player2Score = player2Score + 1;
-                }
-            }            
             System.out.println("Do you want to contniue: (if yes press y)");
             check = scan.next().charAt(0);
             System.out.println();
@@ -118,9 +96,9 @@ public class ExampleCardEnum {
         System.out.println();
 
         System.out.println("PLAYER 2 score: " + player2Score);
-        
+
         System.out.println();
-        
+
         System.out.println("Final score---> ");
         if (player1Score > player2Score) {
             System.out.println("PLAYER 1 won");
