01bc11aea28f9cf6d58abed4f46d4f9a5789d2c2
sin10490 <sin10490@sheridancollege.ca>
2020-08-05 00:28:24 -0400

    Next Commit

--- src/ca/sheridancollege/project/GenerateCard.java
+++ src/ca/sheridancollege/project/GenerateCard.java
@@ -2,7 +2,7 @@
  * Group: Kirat Mehta, Gurjit Singh, Jagdeep Singh, Parminder Kaur Puri
  * 1205_11047 - Fundamentals of Software Design and Development
  * Project name :- Card Game
- * Date July 2020
+ * Date August 2020
  */
 package ca.sheridancollege.project;
 
